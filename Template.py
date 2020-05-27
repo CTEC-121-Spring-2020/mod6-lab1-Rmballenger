8cd3d0d9785e55168b7cc438498e2be21358b0fc
Rmballenger <R.ballenger@students.clark.edu>
2020-05-27 12:19:45 -0700

    FInished lab.

--- Template.py
+++ Template.py
@@ -13,7 +13,36 @@ Output: return value and description
 
 
 def main():
-    # code here
+    '''
+    # definte loop exaple
+    for i in range(0, 11, 2):
+        print(i)
+    print()
+
+    # same behavior but coded as indefinate or while loop
+    count = 0
+    while(count < 11):
+        print(count)
+        #count = count + 2
+        count += 2
+        # count
+
+    x = True
+    y = True
+
+    if(x):
+        if(y):
+            print("both true")
+        else:
+            print("y false")
+    else:
+        print("x false")
+    print()
+    '''
+    print(True and False)
+    print(True and True)
+    print(True and False or False)
+    print(False or False or True)
 
 
 main()
